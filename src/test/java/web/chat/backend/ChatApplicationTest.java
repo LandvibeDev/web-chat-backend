4bd793c44148851f21395193c725a670ffc19c8f
Seung Bin . Ko <kosb15@naver.com>
2020-08-15 14:38:01 +0900

    fix: 데이터베이스 관련 자동 구성을 포함하도록 변경 (#7)
    - DataSourceAutoConfiguration 포함

--- src/test/java/web/chat/backend/ChatApplicationTest.java
+++ src/test/java/web/chat/backend/ChatApplicationTest.java
@@ -2,11 +2,12 @@ package web.chat.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@ActiveProfiles("test")
+@EnableAutoConfiguration
 class ChatApplicationTest {
 
 	@Test
