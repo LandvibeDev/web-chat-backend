5044c58be104f32239c5742799a1396786c48870
Seung Bin . Ko <kosb15@naver.com>
2020-08-15 13:38:26 +0900

    feat: 메시지 리스트 조회 기능 추가 (#7)
    - Message 엔티티에 Room 엔티티와의 ManyToOne 연관 관계 추가
    - Message 엔티티를 List<MessageResponse> 타입으로 변환하는 Collector 추가
      - reference by : https://blog.indrek.io/articles/creating-a-collector-in-java-8/
    - 존재하진 않는 Room Id 인 경우, 400 응답 코드
    - 임시 SQL 데이터에 room_id 필드 추가

--- src/main/java/web/chat/backend/controller/response/MessageResponse.java
+++ src/main/java/web/chat/backend/controller/response/MessageResponse.java
@@ -1,4 +1,19 @@
 package web.chat.backend.controller.response;
 
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import web.chat.backend.entity.MessageType;
+
+@Getter
+@Setter
 public class MessageResponse {
+	private Long id;
+
+	private String contents;
+
+	private LocalDateTime createdAt;
+
+	private MessageType messageType;
 }
--- src/main/java/web/chat/backend/repository/MessageRepository.java
+++ src/main/java/web/chat/backend/repository/MessageRepository.java
@@ -1,4 +1,13 @@
 package web.chat.backend.repository;
 
-public interface MessageRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import web.chat.backend.entity.Message;
+
+public interface MessageRepository extends JpaRepository<Message, Long> {
+	List<Message> findAllByRoomId(Long roomId);
+
+	boolean existsByRoomId(Long roomId);
 }
