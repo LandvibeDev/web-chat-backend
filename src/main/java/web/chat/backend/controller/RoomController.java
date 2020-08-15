45b9bf63d823d8842fdbf2158a9d037ba2e374cd
KwonYeKyeong <kwonliz6.6@gmail.com>
2020-08-16 00:58:36 +0900

    feat: 채팅방 목록 내려주는 api 구현 (#4)

--- src/main/java/web/chat/backend/controller/RoomController.java
+++ src/main/java/web/chat/backend/controller/RoomController.java
@@ -1,4 +1,26 @@
 package web.chat.backend.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import web.chat.backend.entity.Room;
+import web.chat.backend.service.RoomService;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/rooms")
 public class RoomController {
+
+	private final RoomService roomService;
+
+	@GetMapping("")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Room> getRooms(){ return roomService.getRooms(); }
+
 }
--- src/main/java/web/chat/backend/repository/RoomRepository.java
+++ src/main/java/web/chat/backend/repository/RoomRepository.java
@@ -1,4 +1,8 @@
 package web.chat.backend.repository;
 
-public interface RoomRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import web.chat.backend.entity.Room;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
 }
